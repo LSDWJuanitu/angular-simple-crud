f31de5e7a85c7a5a0b683717033e7af3bdc6613a
LSDWJuanitu <144205268+LSDWJuanitu@users.noreply.github.com>
2024-10-01 21:24:12 -0600

    AvanceCRUDAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Registro } from './models/registro';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+
+  registroArray: Registro[] = [
+    {id: 1, nombre: "Juan", pais: "Mexico"},
+    {id: 2, nombre: "Damian", pais: "Mexico"},
+    {id: 3, nombre: "Paulina", pais: "Mexico"}
+  ];
+
+  selectedRegistro: Registro = new Registro();
+
+agregarOeditar(){
+  this.selectedRegistro.id = this.registroArray.length + 1;
+  this.registroArray.push(this.selectedRegistro);
+  this.selectedRegistro = new Registro();
+}
+
+  
+
 }
---
+++ src/app/models/registro.ts
@@ -0,0 +1,5 @@
+export class Registro {
+    id = 0;
+    nombre = "";
+    pais = "";
+}
